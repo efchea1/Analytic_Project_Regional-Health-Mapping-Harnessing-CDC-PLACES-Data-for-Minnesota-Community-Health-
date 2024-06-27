a8096b61119e35e195cc1e943c931d5943967085
Emmanuel Fle Chea <cheax014@umn.edu>
2024-06-26 21:18:31 -0500

    I committed this part of the project.

--- app_prototype.R
+++ app_prototype.R
@@ -1,18 +1,18 @@
 ## Shiny Dashboard
 ## Load Packages ---------------------------------------------------------------
-library(shiny)
-library(readr)
-library(ggplot2)
-library(dplyr)
-library(shinydashboard)
-library(shinyjs)
-library(plotly)
+library(shiny)            # shiny package for building interactive web applications
+library(readr)            # readr package for reading data files
+library(ggplot2)          # ggplot2 package for creating graphics
+library(dplyr)            # dplyr package for data manipulation
+library(shinydashboard)   # shinydashboard package for creating dashboards
+library(shinyjs)          # shinyjs package for adding JavaScript functionality
+library(plotly)           # plotly package for creating interactive plots
 
 # Load Data -------------------------------------------------------------------
 # Minnesota CDC Places Census Estimate data for 2020 to 2022
 CensusEstMN <- read.csv(
   'https://raw.githubusercontent.com/quincountychsmn/Public-Data-Sources/main/CDC%20Places/2020%20to%202022%20Pop.%20Estimates/cc-est2022-agesex.csv'
-)
+) # Load census data from a CSV file available online
 
 # Minnesota Coronary Heart Disease (CHD) data 2018 to 2021
 CHD_files <- list(
@@ -20,31 +20,32 @@ CHD_files <- list(
   'https://raw.githubusercontent.com/quincountychsmn/Public-Data-Sources/main/CDC%20Places/Places%20CDC%20Estimates/CHD/CHD2019.csv',
   'https://raw.githubusercontent.com/quincountychsmn/Public-Data-Sources/main/CDC%20Places/Places%20CDC%20Estimates/CHD/CHD2020.csv',
   'https://raw.githubusercontent.com/quincountychsmn/Public-Data-Sources/main/CDC%20Places/Places%20CDC%20Estimates/CHD/CHD2021.csv'
-)
+) # List of URLs for CHD data files from 2018 to 2021
 
-CHD_data <- lapply(CHD_files, read.csv)
+CHD_data <- lapply(CHD_files, read.csv) # Read each CHD data file into a list of data frames
 
 # CHB, County
 chb_raw <- read.csv(
   'https://raw.githubusercontent.com/quincountychsmn/Public-Data-Sources/main/MN%20SCHSAC%20%26%20CHB%20Regions/Community%20Health%20Board%20as%20of%201_17_2024.csv'
-)
+) # Load Community Health Board (CHB) data from a CSV file
 
 # MN Region
 mn_region_raw <- read.csv(
   'https://raw.githubusercontent.com/quincountychsmn/Public-Data-Sources/main/MN%20SCHSAC%20%26%20CHB%20Regions/State%20Community%20Health%20Services%20Advisory%20Committee%20as%20of%201_17_2024.csv'
-)
+) # Load Minnesota region data from a CSV file
 
 # Data Wrangling --------------------------------------------------------------
 # Clean and merge CHD data
 CHD_data <- lapply(CHD_data, function(df) {
-  df$LocationID <- ifelse("Latitude" %in% colnames(df), as.character(df$Latitude), NA)
-  colnames(df)[colnames(df) == 'Geolocatioin'] <- 'Geolocation'
+  df$LocationID <- ifelse("Latitude" %in% colnames(df), as.character(df$Latitude), NA) # Add LocationID if Latitude column exists
+  colnames(df)[colnames(df) == 'Geolocatioin'] <- 'Geolocation' # Correct spelling of Geolocation column if needed
   df
-})
-CHD_Final <- bind_rows(CHD_data)
+}) # Clean each data frame in the CHD data list
+
+CHD_Final <- bind_rows(CHD_data) # Combine all cleaned CHD data frames into one data frame
 
 # Filter and select locations
-Selected_Locations <- CHD_Final %>%
+Selected_Locations <- CHD_Final |> 
   filter(LocationName %in% c("Aitkin", "Anoka", "Becker", "Beltrami", "Benton", "Big Stone", "Blue Earth", "Brown", "Carlton", "Carver",
                              "Cass", "Chippewa", "Chisago", "Clay", "Clearwater", "Cook", "Cottonwood", "Crow Wing", "Dakota", "Dodge", "Douglas",
                              "Faribault", "Fillmore", "Freeborn", "Goodhue", "Grant", "Hennepin", "Houston", "Hubbard", "Isanti", "Itasca",
@@ -54,36 +55,37 @@ Selected_Locations <- CHD_Final %>%
                              "Red Lake", "Redwood", "Renville", "Rice", "Rock", "Roseau", "Scott", "Sherburne", "Sibley", "St. Louis", "Stearns",
                              "Steele", "Stevens", "Swift", "Todd", "Traverse", "Wabasha", "Wadena", "Waseca", "Washington", "Watonwan",
                              "Wilkin", "Winona", "Wright", "Yellow Medicine"),
-         Year == 2021, StateAbbr == "MN")
+         Year == 2021, StateAbbr == "MN") # Filter CHD data for selected locations and year 2021 in MN
 
 # Clean census data
-CensusEstMN$CTYNAME <- gsub(" County", "", CensusEstMN$CTYNAME)
+CensusEstMN$CTYNAME <- gsub(" County", "", CensusEstMN$CTYNAME) # Remove " County" from county names in census data
 
 # Population estimates for CHD in MN
-PopEst_CHDMN <- CensusEstMN %>%
-  filter(YEAR == 3) %>%
-  inner_join(Selected_Locations, by = c("CTYNAME" = "LocationName")) %>%
-  select(CTYNAME, Data_Value_Type, AGE18PLUS_TOT, Measure, Data_Value, High_Confidence_Limit, Low_Confidence_Limit)
+PopEst_CHDMN <- CensusEstMN |> 
+  filter(YEAR == 3) |> # Filter data for the year 2022 (YEAR == 3)
+  inner_join(Selected_Locations, by = c("CTYNAME" = "LocationName")) |> # Join census data with selected CHD locations
+  select(CTYNAME, Data_Value_Type, AGE18PLUS_TOT, Measure, Data_Value, High_Confidence_Limit, Low_Confidence_Limit) # Select relevant columns
 
 # Calculate crude and age-adjusted values
 calc_aggregate_values <- function(df) {
-  df %>%
+  df |> 
     mutate(Aggregate_Data_Value = Data_Value * AGE18PLUS_TOT / 100,
            Aggregate_Low_Confidence_Limit = Low_Confidence_Limit * AGE18PLUS_TOT / 100,
-           Aggregate_High_Confidence_Limit = High_Confidence_Limit * AGE18PLUS_TOT / 100) %>%
+           Aggregate_High_Confidence_Limit = High_Confidence_Limit * AGE18PLUS_TOT / 100) |> # Calculate aggregate values
     select(CTYNAME, Data_Value_Type, AGE18PLUS_TOT, Aggregate_High_Confidence_Limit,
-           Aggregate_Data_Value, Aggregate_Low_Confidence_Limit)
+           Aggregate_Data_Value, Aggregate_Low_Confidence_Limit) # Select relevant columns
 }
 
-CHD_Crude21MN <- calc_aggregate_values(PopEst_CHDMN %>% filter(Data_Value_Type == 'Crude prevalence'))
-CHD_Adj21MN <- calc_aggregate_values(PopEst_CHDMN %>% filter(Data_Value_Type == 'Age-adjusted prevalence'))
-CHD_MN21 <- bind_rows(CHD_Adj21MN, CHD_Crude21MN)
+CHD_Crude21MN <- calc_aggregate_values(PopEst_CHDMN |> filter(Data_Value_Type == 'Crude prevalence')) # Calculate crude values
+CHD_Adj21MN <- calc_aggregate_values(PopEst_CHDMN |> filter(Data_Value_Type == 'Age-adjusted prevalence')) # Calculate age-adjusted values
+CHD_MN21 <- bind_rows(CHD_Adj21MN, CHD_Crude21MN) # Combine crude and age-adjusted values
 
+# Define UI -------------------------------------------------------------------
 ui <- dashboardPage(
   dashboardHeader(
     title = "CDC Places to MN Regions",
     titleWidth = 400
-  ),
+  ), # Dashboard header with title
   dashboardSidebar(
     width = 350,
     selectInput(
@@ -92,15 +94,15 @@ ui <- dashboardPage(
       choices = sort(unique(mn_region_raw$County)),
       selected = "Kittson",
       width = 350
-    ),
+    ), # Sidebar with county selection input
     sidebarMenu(
       menuItem("Home", tabName = "tn_homePage"),
       menuItem("Region & CHB Definition", tabName = "tn_regionChbDefinitions"),
       menuItem("Coronary Heart Disease", tabName = "tn_coronaryHeartDisease")
-    )
+    ) # Sidebar menu with navigation items
   ),
   dashboardBody(
-    shinyjs::useShinyjs(),
+    shinyjs::useShinyjs(), # Enable shinyjs for JavaScript functionality
     tabItems(
       tabItem(
         tabName = "tn_homePage",
@@ -121,7 +123,7 @@ ui <- dashboardPage(
                 tags$h3("Those involved with this project are:"),
                 tags$h4("Emmanuel Fle Chea, MPH, Public Health Data Science, University of Minnesota School of Public Health"),
                 tags$h4("Mr. Patrick Olson (Preceptor), Quin County Community Health Board, Community Resource Liaison/Associate/Researcher")
-              )
+              ) # Home page content with welcome message and project details
             )
           )
         )
@@ -141,23 +143,23 @@ ui <- dashboardPage(
                     h3(HTML("Updating the Select County of Interest filter will highlight the county in <font color=red>red</font> while the Regions and Community Health Boards will remain in <b>bold</b>.")),
                     h3("For this tab, the Select SCHSAC Region and Select Community Health Board filters are greyed out because they do not execute any function on this tab."),
                     h3("The purpose for this tab is to provide a quick reference for what counties fall under which region and Community Health Board."),
-                    tags$hr(style = "border-top: 1px solid #ccc; margin-top: 20px; margin-bottom: 20px;")
+                    tags$hr(style = "border-top: 1px solid #ccc; margin-top: 20px; margin-bottom: 20px;") # Instructions and purpose for Region/CHB tab
                   )
                 ),
                 fluidRow(
-                  column(6, uiOutput("region_narrative", style = "font-size: 20px;")),
-                  column(6, uiOutput("chb_narrative_01", style = "font-size: 20px;"))
+                  column(6, uiOutput("region_narrative", style = "font-size: 20px;")), # Region narrative output
+                  column(6, uiOutput("chb_narrative_01", style = "font-size: 20px;")) # CHB narrative output
                 ),
                 fluidRow(
                   column(
                     width = 12,
                     h3("Regions and Counties"),
-                    uiOutput("region_counties"),
+                    uiOutput("region_counties"), # UI output for regions and counties list
                   ),
                   column(
                     width = 12,
                     h3("Community Health Boards"),
-                    uiOutput("chb_counties"),
+                    uiOutput("chb_counties"), # UI output for CHB and counties list
                   )
                 )
               )
@@ -179,14 +181,14 @@ ui <- dashboardPage(
                   label = "Select Year",
                   choices = sort(unique(Selected_Locations$Year), decreasing = TRUE),
                   selected = max(unique(Selected_Locations$Year))
-                ),
+                ), # Year selection input for CHD data
                 selectInput(
                   "par_leadStateRegionChb",
                   label = "Select Comparison",
                   choices = c("All", "State", "Region", "Community Health Board"),
                   selected = "All",
                   multiple = FALSE
-                ),
+                ), # Comparison selection input for CHD data
                 fluidRow(
                   column(
                     width = 10,
@@ -195,14 +197,14 @@ ui <- dashboardPage(
                       status = "primary",
                       solidHeader = TRUE,
                       collapsible = TRUE,
-                      plotOutput("plot_chdEstimate")
+                      plotOutput("plot_chdEstimate") # Plot output for CHD exposure estimate
                     ),
                     box(
                       title = "Confidence Interval for Coronary Heart Disease Estimate",
                       status = "primary",
                       solidHeader = TRUE,
                       collapsible = TRUE,
-                      plotOutput("plot_confidenceInterval")
+                      plotOutput("plot_confidenceInterval") # Plot output for CHD confidence interval
                     )
                   )
                 )
@@ -222,7 +224,7 @@ server <- function(input, output, session) {
       session,
       "parGlobal_region",
       choices = unique(mn_region_raw$RegionName)
-    )
+    ) # Update region input choices based on unique region names in the data
   })
   
   observe({
@@ -230,83 +232,83 @@ server <- function(input, output, session) {
       session,
       "parGlobal_chb",
       choices = unique(chb_raw$CHBName)
-    )
+    ) # Update CHB input choices based on unique CHB names in the data
   })
   
   output$region_narrative <- renderUI({
-    filtered_region <- mn_region_raw %>%
-      filter(County == input$parGlobal_county)
+    filtered_region <- mn_region_raw |>
+      filter(County == input$parGlobal_county) # Filter region data for the selected county
     HTML(
       paste0(
         "<b>", unique(filtered_region$RegionName), " Region</b> is made up of the following counties: ",
         paste(unique(filtered_region$County), collapse = ", "), "."
-      )
+      ) # Create HTML content for displaying the region narrative
     )
   })
   
   output$chb_narrative_01 <- renderUI({
-    filtered_chb <- chb_raw %>%
-      filter(County == input$parGlobal_county)
+    filtered_chb <- chb_raw |>
+      filter(County == input$parGlobal_county) # Filter CHB data for the selected county
     HTML(
       paste0(
         "<b>", unique(filtered_chb$CHBName), " Community Health Board</b> includes: ",
         paste(unique(filtered_chb$County), collapse = ", "), "."
-      )
+      ) # Create HTML content for displaying the CHB narrative
     )
   })
   
   # Helper function to highlight selected county in text
   highlight_county <- function(text, selected_county) {
-    gsub(selected_county, paste0("<font color='red'>", selected_county, "</font>"), text)
+    gsub(selected_county, paste0("<font color='red'>", selected_county, "</font>"), text) # Highlight the selected county in red
   }
   
   output$region_counties <- renderUI({
     selected_county <- input$parGlobal_county
-    regions <- mn_region_raw %>%
-      group_by(Region) %>%
-      summarise(Counties = paste(County, collapse = ", "))
+    regions <- mn_region_raw |>
+      group_by(Region) |>
+      summarise(Counties = paste(County, collapse = ", ")) # Group data by region and concatenate county names
     
-    regions_text <- regions %>%
-      mutate(Text = paste0(Region, " Region:: ", Counties)) %>%
-      pull(Text)
+    regions_text <- regions |>
+      mutate(Text = paste0("<b>", Region, " Region::</b> ", Counties)) |>
+      pull(Text) # Create HTML text with bold region names
     
-    regions_text <- sapply(regions_text, highlight_county, selected_county = selected_county)
-    HTML(paste(regions_text, collapse = "<br>"))
+    regions_text <- sapply(regions_text, highlight_county, selected_county = selected_county) # Highlight selected county in the regions text
+    HTML(paste(regions_text, collapse = "<br>")) # Render HTML for regions and counties
   })
   
   output$chb_counties <- renderUI({
     selected_county <- input$parGlobal_county
-    chbs <- chb_raw %>%
-      group_by(CHB) %>%
-      summarise(Counties = paste(County, collapse = ", "))
+    chbs <- chb_raw |>
+      group_by(CHB) |>
+      summarise(Counties = paste(County, collapse = ", ")) # Group data by CHB and concatenate county names
     
-    chb_text <- chbs %>%
-      mutate(Text = paste0(CHB, ":: ", Counties)) %>%
-      pull(Text)
+    chb_text <- chbs |>
+      mutate(Text = paste0("<b>", CHB, "::</b> ", Counties)) |>
+      pull(Text) # Create HTML text with bold CHB names
     
-    chb_text <- sapply(chb_text, highlight_county, selected_county = selected_county)
-    HTML(paste(chb_text, collapse = "<br>"))
+    chb_text <- sapply(chb_text, highlight_county, selected_county = selected_county) # Highlight selected county in the CHB text
+    HTML(paste(chb_text, collapse = "<br>")) # Render HTML for CHBs and counties
   })
   
-  # Reactive Data
+  # Reactive Data for plotting
   reactive_CHD_data <- reactive({
-    CHD_MN21 %>% filter(CTYNAME == input$parGlobal_county)
+    CHD_MN21 |> filter(CTYNAME == input$parGlobal_county) # Filter CHD data for the selected county
   })
   
   output$plot_chdEstimate <- renderPlot({
-    data <- reactive_CHD_data()
+    data <- reactive_CHD_data() # Get the filtered CHD data
     ggplot(data, aes(x = CTYNAME, y = Aggregate_Data_Value, fill = Data_Value_Type)) +
       geom_bar(stat = "identity", position = "dodge") +
-      labs(title = "Coronary Heart Disease Exposure Estimate", x = "County", y = "Estimate")
+      labs(title = "Coronary Heart Disease Exposure Estimate", x = "County", y = "Estimate") # Plot CHD exposure estimate
   })
   
   output$plot_confidenceInterval <- renderPlot({
-    data <- reactive_CHD_data()
+    data <- reactive_CHD_data() # Get the filtered CHD data
     ggplot(data, aes(x = CTYNAME, y = Aggregate_Data_Value, color = Data_Value_Type)) +
       geom_errorbar(aes(ymin = Aggregate_Low_Confidence_Limit, ymax = Aggregate_High_Confidence_Limit), width = 0.2) +
-      labs(title = "Confidence Interval for Coronary Heart Disease Estimate", x = "County", y = "Estimate")
+      labs(title = "Confidence Interval for Coronary Heart Disease Estimate", x = "County", y = "Estimate") # Plot CHD confidence interval
   })
 }
 
 # Run the app -----------------------------------------------------------------
-shinyApp(ui = ui, server = server) 
+shinyApp(ui = ui, server = server) # Run the Shiny application 
